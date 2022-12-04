4d2c6be0e66bc541d557d9f4283e5eb689d50366
Peter Qiu <273743552@qq.com>
2022-12-04 13:24:34 +0800

    feat: support preview

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ async function run() {
     const actionType = core.getInput('action_type') || 'preview';
     const projectPath = core.getInput('project_path') || './';
     const version = core.getInput('version') || '1.0.0';
+    const ignores = core.getInput('ignores') || [];
+    const desc = core.getInput('desc') || '自动上传版本';
     const { MINI_APP_ID, MINI_APP_PRIVATE_KEY, GITHUB_WORKSPACE: sourceDir = '' } = process.env;
     const uploadDir = path.join(sourceDir, projectPath);
 
@@ -22,10 +24,21 @@ async function run() {
       projectPath: uploadDir,
       privateKeyPath,
       type: projectType,
+      ignores,
     });
+    const optional = {};
+    const qrcodeDesc = path.resolve(sourceDir, 'preview.jpg');
+
+    if (actionType == 'preview') {
+      optional.qrcodeFormat = core.getInput('qrcodeFormat') || 'base64';
+      optional.qrcodeOutputDest = qrcodeDesc;
+    }
+
     const ans = await ci[actionType]({
       project,
       version,
+      desc,
+      ...optional,
       setting: {
         es6: core.getInput('es6'),
         es7: core.getInput('es7'),
@@ -34,7 +47,7 @@ async function run() {
     });
 
     console.log('ci result: ', ans);
-    core.setOutput('preview_pic_dir', '');
+    core.setOutput('preview-qrcode', fs.readFileSync(qrcodeDesc, { encoding: 'utf-8'}));
   } catch (error) {
     console.log('catch error', error);
     core.setFailed(error);
